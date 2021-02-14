b068ae2a7d017c1fd4f1cba611da1db15a435e86
Saeem03 <Saeem.shanto@northsouth.edu>
2021-02-14 22:53:33 +0600

    First Push

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import PersonList from './GetData';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <PersonList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
