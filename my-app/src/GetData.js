378883dedc6b7f1cfdcabf0e5e8f1a490b58b13a
Saeem03 <Saeem.shanto@northsouth.edu>
2021-02-14 23:13:00 +0600

    3

--- my-app/src/GetData.js
+++ my-app/src/GetData.js
@@ -6,11 +6,12 @@ export default class PersonList extends React.Component {
   state = {
     persons: []
   }
-    setState: any;
+    // setState: any;
 
   componentDidMount() {
-    axios.get(`https://raw.githubusercontent.com/Saeem03/React-App/main/my-app/Data/Data.json?token=AMKT4LOFBA7TRHAGJNMXR5TAGKIJK`)
+    axios.get(`https://raw.githubusercontent.com/Saeem03/React-App/main/my-app/Data/Data.json?token=AMKT4LPLZH5ZAVRYVYV7SZ3AGKJTC`)
       .then(res => {
+          console.log(this.state.persons);
         const persons = res.data;
         this.setState({ persons });
         console.log(persons);
@@ -19,9 +20,12 @@ export default class PersonList extends React.Component {
 
   render() {
     return (
+        <div>
+
       <ul>
-        {/* { this.state.persons.map(person => <li>{person.name}</li>)} */}
+        { this.state.persons.map(person => <li>{person.name}</li>)}
       </ul>
+        </div>
     )
   }
 }
